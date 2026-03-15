6c9e1bfc2c42644be128ad4dd7c0fce8b4c72302
brandorb73-ship-it <brandorb73@gmail.com>
2026-03-15 23:02:19 +1100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,14 @@
+import Head from 'next/head'
+import Dashboard from '../components/Dashboard'
 
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>TradeShield AI | Fraud Detection</title>
+        <meta name="description" content="Circular trade and fraud detection dashboard" />
+      </Head>
+      <Dashboard />
+    </div>
+  )
+}
