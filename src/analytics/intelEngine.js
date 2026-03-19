11fd38e8ff601b8eedcd1bbccc1d2b02a861363a
brandorb73-ship-it <brandorb73@gmail.com>
2026-03-19 17:31:37 +1100

    Create intelEngine.js

---
+++ src/analytics/intelEngine.js
@@ -0,0 +1,32 @@
+import detectFraudRings from "./fraudRings";
+import detectCycles from "./cycleDetection";
+import { calculateShellProbability } from "./shellProbability";
+import detectCorridors from "./corridorHeatmap";
+import detectMLAnomaly from "./mlAnomaly";
+import invoiceCheck from "./invoiceCheck";
+
+export default function runIntelEngine(data) {
+
+  let rings = [];
+  let cycles = [];
+  let shellScores = {};
+  let corridors = [];
+  let anomalies = [];
+  let invoiceFlags = [];
+
+  try { rings = detectFraudRings(data); } catch(e){}
+  try { cycles = detectCycles(data); } catch(e){}
+  try { shellScores = calculateShellProbability(data); } catch(e){}
+  try { corridors = detectCorridors(data); } catch(e){}
+  try { anomalies = detectMLAnomaly(data); } catch(e){}
+  try { invoiceFlags = invoiceCheck(data); } catch(e){}
+
+  return {
+    rings,
+    cycles,
+    shellScores,
+    corridors,
+    anomalies,
+    invoiceFlags
+  };
+}
